9a82c6316a4621930c69c45053d5bc9b9fddf600
Gerardo Vela <Gerardo Vela>
2016-11-25 14:48:57 -0700

    added login and reg folders

---
+++ registration/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bulletins</title>
+	<link rel="stylesheet" type="text/css" href="../lib/main.css">
+</head>
+<body>
+
+<div class="header">
+	<ul>
+		<a href="#"><li>Alumni</li></a>
+		<a href="#"><li>Profile</li></a>
+		<a href="#"><li>Log In</li></a>
+		<a href="#"><li>Register</li></a>
+	</ul>
+</div>
+
+Registration
+
+</body>
+</html>
