662b54be66abb989c215de6d99f1947fa752d2de
Jesus Borunda <jesus.e.borunda@gmail.com>
2016-11-28 16:54:52 -0700

    Added main layout of bulletins page

--- bulletins/index.php
+++ bulletins/index.php
@@ -6,20 +6,58 @@
 </head>
 <body>
 
-<div class="header">
-	<ul>
-		<a href="#"><li>Alumni</li></a>
-		<a href="#"><li>Profile</li></a>
-		<a href="#"><li>Log In</li></a>
-		<a href="#"><li>Register</li></a>
-	</ul>
-</div>
-
-<div class="main-content">
-	<div class="bulletin-wrapper">
-		
+	<div class="header">
+		<ul>
+			<a href="#"><li>Alumni</li></a>
+			<a href="#"><li>Profile</li></a>
+			<a href="#"><li>Log In</li></a>
+			<a href="#"><li>Register</li></a>
+		</ul>
 	</div>
-</div>
+
+	<div class="main-content">
+		<div class="bulletin-buttons">
+			<button>My Bulletins</button>
+			<button>New Bulletin</button>
+		</div>
+		<div class="bulletin-wrapper">
+			<h3>Bulletins</h3>
+			<div class="bulletins">
+				<div class="bulletin">
+					<h4>Title of bulletin</h4>
+					<div class="bulletin-content">
+						Sample bulletin example
+					</div>
+					<div class="creator">Creator of thhe bulletin: Jesus</div>
+					<div class="date">Date bulletin was created: 11-28-2016</div>
+				</div>
+				<div class="bulletin">
+					<h4>Title of bulletin</h4>
+					<div class="bulletin-content">
+						Sample bulletin example
+					</div>
+					<div class="creator">Creator of thhe bulletin: Jesus</div>
+					<div class="date">Date bulletin was created: 11-28-2016</div>
+				</div>
+				<div class="bulletin">
+					<h4>Title of bulletin</h4>
+					<div class="bulletin-content">
+						Sample bulletin example
+					</div>
+					<div class="creator">Creator of thhe bulletin: Jesus</div>
+					<div class="date">Date bulletin was created: 11-28-2016</div>
+				</div>
+				<div class="bulletin">
+					<h4>Title of bulletin</h4>
+					<div class="bulletin-content">
+						Sample bulletin example
+					</div>
+					<div class="creator">Creator of thhe bulletin: Jesus</div>
+					<div class="date">Date bulletin was created: 11-28-2016</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
 </body>
 </html>
