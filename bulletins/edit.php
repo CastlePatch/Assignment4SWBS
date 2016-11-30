ef486d774acc3e3d0b6d1ef3903253fd57b335fb
Jesus Borunda <jesus.e.borunda@gmail.com>
2016-11-30 15:38:43 -0700

    fixed buttons

--- bulletins/edit.php
+++ bulletins/edit.php
@@ -17,12 +17,12 @@
 
 	<div class="main-content">
 		<div class="bulletin-buttons">
-			<button onclick="window.location = 'mybulletins.php'">My Bulletins</button>
+			<button onclick="window.location = 'mybulletins.php'">New Bulletin</button>
 			<button onclick="window.location = 'new.php'">New Bulletin</button>
 		</div>
 		<div class="bulletin-input">
 			<h3>Bulletins</h3>
-			<form action="new.php" method="POST">
+			<form action="edit.php" method="POST">
 				<div>
 					<div>Title:</div>
 					<input type="text" name="Title">
@@ -31,6 +31,7 @@
 					<div>Content:</div>
 					<textarea type="text" name="Content"></textarea>
 				</div>
+				<input type="hidden" name="id" value="<?php echo $id; ?>">
 				<button type="submit">Submit</button>
 			</form>
 		</div>
